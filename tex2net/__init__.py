b98652c59aee606f855b23e79b1b1a7e11e4056c
Joao Aparicio <joao.aparicio@tecnico.ulisboa.pt>
2025-03-29 00:16:04 +0000

    Update __init__.py

--- tex2net/__init__.py
+++ tex2net/__init__.py
@@ -9,6 +9,16 @@ from .analysis import (
     analyze_graph_characteristics,
     detect_communities,
     calculate_degree_centrality,
+    compare_node_similarity,
+    compare_edge_similarity,
+    compare_degree_distributions,
+    compare_average_clustering,
+    compute_graph_edit_distance,
+    compare_graph_density,
+    compare_avg_shortest_path,
+    compare_modularity,
+    compare_betweenness_correlations,
+    compare_graphs
 )
-from .visualization import visualize_graph, visualize_pyvis_graph, visualize_directed_graph, visualize_directed_graph_styled, visualize_directed_graph_styled_communities
+from .visualization import visualize_graph, visualize_pyvis_graph, visualize_directed_graph, visualize_directed_graph_styled, visualize_directed_graph_styled_communities, visualize_interaction_temporality
 
